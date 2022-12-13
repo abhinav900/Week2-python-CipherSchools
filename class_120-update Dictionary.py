20f0c0aa077beb037e47340f60f58f0af949dd5e
Abhinav Kumar <114800265+abhinav900@users.noreply.github.com>
2022-12-14 00:14:55 +0530

    KOC28_CipherSchools

---
+++ class_120-update Dictionary.py
@@ -0,0 +1,12 @@
+# update method
+
+user_info={
+    'name':'Abhinav',
+    'age': 24,
+    'fav_movies': ['avengers'],
+    'fav_tunes': ['awakening','fairy tale'],
+}
+
+more_info= {'state' : 'up', 'hobbies' : ['coding','reading','gaming']}
+user_info.update({})
+print(user_info)
